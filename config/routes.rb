6e25ebffb8c95cdb92202728bae9cd69243ba3be
Bastekkkk <bastian.jarosz@gmail.com>
2018-10-22 21:27:18 +0200

    Update Sotck

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'welcome#index'
+  root 'users#my_portfolio'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'stock_search', to: "stocks#search"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
