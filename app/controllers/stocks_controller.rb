8e40e9049afb74927479a3b3b5f0b21cb6b64ea6
Bastekkkk <bastian.jarosz@gmail.com>
2018-10-25 15:16:40 +0200

    add

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,17 +1,51 @@
 class StocksController < ApplicationController
 
 def search
-if params[:stock].present?
-  @stock = Stock.new_form_lookup(params[:stock])
-  if @stock
-    render 'users/my_portfolio'
+
+  if params[:stock].blank?
+    flash.now[:danger] = "You have entered an empety string"
   else
-    flash[:danger] = "you have entered  an incorrect symbol"
-    redirect_to my_portfolio_path
+    @stock = Stock.new_form_lookup(params[:stock])
+    flash.now[:danger] = "You have entered incorrect stock symbol" unless @stock
   end
-else
-  flash[:danger] = "you have entered empety string"
+    respond_to do |format|
+      format.js {render partial: 'users/results'}
+  end
+end
+
+def destory
+  stock = Stock.find(params[:id])
+  @user_stock = UserStock.where(user_id: current_user, stock_id: stock.id).first
+  @user_stock.destroy
+  flash[:success] = "Stack successfully removed from portfolio"
   redirect_to my_portfolio_path
+
 end
-end
+
+
+
+
+
+#def search
+#if params[:stock].present?
+#  @stock = Stock.new_form_lookup(params[:stock])
+#  if @stock
+#    respond_to do |format|
+#    format.js {render partial: 'users/results'}
+#  end
+#  else
+#    respond_to do |format|
+#    flash.now[:danger] = "you have entered  an incorrect symbol"
+#    format.js {render partial: 'users/results'}
+#  end
+#  end
+#else
+#  respond_to do |format|
+#  flash.now[:danger] = "you have entered empety search string"
+#  format.js {render partial: 'users/results'}
+#end
+#end
+#end
+
+
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,9 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users ,through: :user_stocks
 
   def self.new_form_lookup(ticker_symbol)
-
 #begin .... rescue Exception => e : ratowanie przed errror
-
   begin
    looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
    #price = looked_up_stock.latest_price
@@ -13,9 +13,12 @@ class Stock < ApplicationRecord
  rescue Exception => e
   p e.inspect
   return nil
-
-
   end
 end
+
+def self.find_by_ticker(ticker_symbol)
+  where(ticker: ticker_symbol).first
+  end
+
 
 end
